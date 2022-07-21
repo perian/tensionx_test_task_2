6c50704dd1fb28c265f0b7c6c9878eda511ea661
perian <dmperia@gmail.com>
2022-07-21 14:13:56 +0300

    Поправил сетку таблицы и навигации

--- src/compoments/inner-table/inner-table-row.tsx
+++ src/compoments/inner-table/inner-table-row.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Test } from '../types/consts';
 import { averageScoreColor } from '../utils/utils';
 import './inner-table.scss';
@@ -9,6 +10,7 @@ type InnerTableProps = {
 
 function InnerTableRow({ test, index }: InnerTableProps) {
   const { absent, concept, date, expSpeed, label, score, speed, total } = test;
+  const [isAbsent, setIsAbsent] = useState(absent);
 
   const scoreTextColor = () => {
     const maxScore = parseInt(label.slice(10));
@@ -17,19 +19,40 @@ function InnerTableRow({ test, index }: InnerTableProps) {
     return averageScoreColor(averageScore);
   };
 
+  function setRowDisabled(): any {
+    const className = 'results-table__tr';
+
+    if (isAbsent) {
+      return `${className}  ${className}--disabled`;
+    }
+    return className;
+  }
+
   return (
-    <div className="results-table__tr">
+    <div className={setRowDisabled()}>
       <p className="results-table__td-number">{index}</p>
       <p className="results-table__td-test-label">{label}</p>
       <p className={`results-table__td-score  ${scoreTextColor()}`}>{score}</p>
-      <p className="results-table__td-speed">{speed}</p>
+      <p className="results-table__td-speed  text-color  text-color--blue">
+        {speed}
+      </p>
       <p className="results-table__td-total-questions">{total}</p>
       <p className="results-table__td-exp-speed">{expSpeed}</p>
       <p className="results-table__td-concept">{concept}</p>
       <p className="results-table__td-date">{date}</p>
-      <label className="results-table__td-absent">
-        <input className="results-table__chekbox" type="checkbox"></input>
-      </label>
+      <div className="results-table__td-absent">
+        <input
+          className="results-table__td-checkbox-input  visually-hidden"
+          type="checkbox"
+          id={`absent-checkbox-${index}`}
+          onChange={() => setIsAbsent((prev) => !prev)}
+          checked={isAbsent}
+        ></input>
+        <label
+          className="results-table__td-checkbox-label"
+          htmlFor={`absent-checkbox-${index}`}
+        ></label>
+      </div>
     </div>
   );
 }
